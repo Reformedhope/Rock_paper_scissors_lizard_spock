060ee5165eb7d60f1cfe0e1d36fafb62e558d713
*reformedhope* <steph_salzburg@hotmail.com>
2023-01-27 11:13:07 -0500

    updated game.py and player.py file

--- game.py
+++ game.py
@@ -11,19 +11,79 @@ class Game:
     def display_rules(self):
         print('Select how many people are going to play, Option 1, 2, or if you select 3 some hocus pocus harry potter magic will happen')
         print(' The game will be played with best 2 out of 3. choose your options wisely.')
+    
+    
     def run_game(self):
+
+        
+
+
         while self.human.score >= 0 and self.ai.score >=0:
             
-            if self.human.choose_gesture == "scissors":
+            if self.human.choose_gesture == "rock" and self.ai.choose_gesture == "scissors ":
+                print("Rock smashed the scissors")
+            elif self.human.choose_gesture == "scissors" and self.ai.choose_gesture == "rock":
+                print("Rock smashed the scissors")
+            elif self.human.choose_gesture == "scissors" and self.ai.choose_gesture == "paper":
+                print("scissors cuts paper")
+            elif self.human.choose_gesture == "paper" and self.ai.choose_gesture == "scissors":
+                print("scissors cuts paper")
+            elif self.human.choose_gesture == "paper" and self.ai.choose_gesture == "rock":
+                print("paper covers rock")
+            elif self.human.choose_gesture == "rock" and self.ai.choose_gesture == "paper":
+                print("paper covers rock")
+            elif self.human.choose_gesture == "rock" and self.ai.choose_gesture == "lizard":
+                print("rock crushes lizard")
+            elif self.human.choose_gesture == "lizard" and self.ai.choose_gesture == "rock":
+                print("rock crushes lizard")
+            elif self.human.choose_gesture == "lizard" and self.ai.choose_gesture == "spock":
+                print("lizard poisons spock")
+            elif self.human.choose_gesture == "spock" and self.ai.choose_gesture == "lizard":
+                print("lizard poisons spock")
+            elif self.human.choose_gesture == "spock" and self.ai.choose_gesture == "Scissors":
+                print("spock smashes scissors")
+            elif self.human.choose_gesture == "scissors" and self.ai.choose_gesture == "spock":
+                print("spock smashes scissors")
+            elif self.human.choose_gesture == "scissors" and self.ai.choose_gesture == "lizard":
+                print("scissors decapitates lizard")
+            elif self.human.choose_gesture == "lizard" and self.ai.choose_gesture == "scissors":
+                print("scissors decapitates lizard")
+            elif self.human.choose_gesture == "lizard" and self.ai.choose_gesture == "paper":
+                print("lizard eats paper")
+            elif self.human.choose_gesture == "paper" and self.ai.choose_gesture == "spock":
+                print("paper disproves spock")
+            elif self.human.choose_gesture == "spock" and self.ai.choose_gesture == "paper":
+                print("paper disproves")
+            elif self.human.choose_gesture == "spock" and self.ai.choose_gesture == "rock":
+                print("spock vaporized rock")
+            elif self.human.choose_gesture == "rock" and self.ai.choose_gesture == "spock":
+                print("spock vaporizes rock")
+            else:
+                print("tie, try again.")
 
-
-
-
-    # def display_winner(self):
-    #     pass
-
-
-
-
-       
     
+             
+
+            #main battle above, duplicates below. 
+
+            # elif self.human.choose_gesture == "rock" and self.ai.choose_gesture == "rock":
+            #     print("Tie, try again ")
+            # elif self.human.choose_gesture == "scissors" and self.ai.choose_gesture == "scissors":
+            #     print("Tie, try again")
+            # elif self.human.choose_gesture == "paper" and self.ai.choose_gesture == "paper":
+            #     print("Tie, try again")
+            # elif self.human.choose_gesture == "spock" and self.ai.choose_gesture == "spock":
+            #     print("Tie, try again")
+            # elif self.human.choose_gesture == "lizard" and self.ai.choose_gesture == "lizard":
+            #     print("Tie, try again")  
+            
+              
+
+
+
+    def display_winner(self):
+        pass
+
+
+
+                
--- player.py
+++ player.py
@@ -1,9 +1,11 @@
 from human import Human
 
 class Player: 
-    def __init__(self, name, ):
+    def __init__(self):
         self.name = input("What is the players name? ")
-        print(f'{self.name} has entered the game' )
+        print(f'{self.name} has entered the game. please select your method of attack')
+        print (gesture_list)
+
 
     def __init__(self) -> None:
         pass
